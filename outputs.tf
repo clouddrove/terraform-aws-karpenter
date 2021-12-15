7a369a557bb13ee03fe39726cb3261511dd8cbe2
Sohan Yadav <31764946+sohanyadav@users.noreply.github.com>
2021-12-15 22:54:27 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "chart" {
   value       = join("", helm_release.karpenter.*.chart)
-  description = "Chart name to be installed. The chart name can be local path, a URL to a chart or the name of the chart if repository is specified. It is also possible to use the <repository>/<chart> format here if you are running Terraform on a system that the repository has been added to with helm repo add but this is not."
 }
+
 output "repository" {
   value       = join("", helm_release.karpenter.*.repository)
   description = "Repository URL where to locate the requested chart."
