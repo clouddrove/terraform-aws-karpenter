bb6c1db8a551b68fe10a924e1ee1d48ec2d5f374
Mahesh Yadav <mahesh.yadav@clouddrove.com>
2021-12-16 14:52:32 +0530

    fix readme

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "chart" {
   value       = join("", helm_release.karpenter.*.chart)
   description = "Chart name to be installed. The chart name can be local path, a URL to a chart, or the name of the chart if repository is specified. It is also possible to use the <repository>/<chart> format here if you are running Terraform on a system that the repository has been added to with helm repo add but this is not."
 }
-output "repository" {
-  value       = join("", helm_release.karpenter.*.repository)
-  description = "Repository URL where to locate the requested chart."
-}
 output "version" {
   value       = join("", helm_release.karpenter.*.version)
   description = "Specify the exact chart version to install. If this is not specified, the latest version is installed."
