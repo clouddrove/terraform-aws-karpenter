efc745d37fb61cc1648f8b71bb4a36715962783d
theprashantyadav <theprashantyadav7@gmail.com>
2023-07-31 15:20:17 +0530

    feat: added new vpc tag

--- _example/example.tf
+++ _example/example.tf
@@ -12,7 +12,7 @@ locals {
 
 module "vpc" {
   source  = "clouddrove/vpc/aws"
-  version = "1.3.1"
+  version = "2.0.0"
   name        = "vpc"
   environment = "test"
   label_order = ["environment", "name"]
