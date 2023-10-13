12ab832103b643865096375099df08d241027f6f
theprashantyadav <79893616+theprashantyadav@users.noreply.github.com>
2023-10-13 22:37:50 +0530

    Fix tflint and added new github action (#30)
    * feat: fix tflint and added new github action
    
    * feat: fix tflint and added new github action
    
    * feat: fix tflint and added new github action
    
    * feat: fix tflint and added new github action
    
    * feat: fix tflint and added new github action
    
    * feat: fix tflint and added new github action
    
    * feat: fix tflint and added new github action
    
    * feat: fix tflint and added new github action
    
    * feat: fix tflint and added new github action
    
    * feat: fix tflint and added new github action
    
    * feat: fix tflint and added new github action
    
    * feat: fix tflint and added new github action
    
    * feat: added k8s version
    
    * feat: added k8s version

--- _example/versions.tf
+++ _example/versions.tf
@@ -7,9 +7,9 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.18.1"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.0.0"
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23.0"
     }
   }
 }
