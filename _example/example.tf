8d37bc45b493997d305ee88d98602d5ff4b5fc72
theprashantyadav <theprashantyadav7@gmail.com>
2023-10-18 14:11:15 +0530

    feat: updated readme.md and github-action version

--- _example/example.tf
+++ _example/example.tf
@@ -251,12 +251,12 @@ provider "kubernetes" {
 module "karpenter" {
   source = "../"
 
-  name              = "${local.name}-karpenter"
-  environment       = local.environment
-  namespace         = "test"
-  create_namespace  = true
-  karpenter_version = "0.31.1"
-  cluster_name      = module.eks.cluster_name
-  depends_on        = [module.eks]
+  name                     = "${local.name}-karpenter"
+  environment              = local.environment
+  namespace                = "test"
+  create_namespace         = true
+  karpenter_version        = "0.31.1"
+  cluster_name             = module.eks.cluster_name
+  depends_on               = [module.eks]
   eks_worker_iam_role_name = "arn:aws:iam::xxxxxxxxxxxx:role/KarpenterControllerRole-clouddrove-Karpenter"
 }
