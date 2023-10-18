27b0012c00db62cb9ea6c598219941cf6c654f56
theprashantyadav <theprashantyadav7@gmail.com>
2023-10-18 16:42:10 +0530

    feat: fixed tflint issue

--- _example/example.tf
+++ _example/example.tf
@@ -258,5 +258,4 @@ module "karpenter" {
   karpenter_version        = "0.31.1"
   cluster_name             = module.eks.cluster_name
   depends_on               = [module.eks]
-  eks_worker_iam_role_name = "arn:aws:iam::xxxxxx:role/karpenter"
 }
